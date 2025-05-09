9e8e4666705e4d705bdf431f5bb85ee354a97a09
louis <louis.masih2015@gmail.com>
2025-05-09 16:20:28 +0400

    Fixed size,height and width

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 pygame.init()
 width= 800
 height = 600
-window_screen=pygame.display.set_mode(width,height)
+window_screen=pygame.display.set_mode(size=(width,height))
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 fps=60
@@ -14,7 +14,7 @@ while True:
     fps_controller = pygame.time.Clock()
     fps_controller.tick(fps)
     for event in pygame.event.get():
-        if event.key == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 print ("space pressed")
         if event.type == pygame.QUIT:
